def80b9831325fddba1d0ec38b009c819afdbc3b
ctnkaan <kaan.taskingenc@gmail.com>
2023-10-27 20:29:19 +0300

    projeme basladim

---
+++ script.js
@@ -0,0 +1,28 @@
+const catGifContainer = document.getElementById("catGifContainer");
+const catGif = document.getElementById("catGif");
+const showCatButton = document.getElementById("showCatButton");
+
+const catGifFiles = [
+  "cat1.gif",
+  "cat2.gif",
+  "cat3.jpg",
+  "cat4.gif",
+  "cat5.gif",
+  "cat6.gif"
+];
+
+let last = ""
+
+showCatButton.addEventListener("click", function () {
+   let randomIndex;
+
+  do {
+     randomIndex = Math.floor(Math.random() * catGifFiles.length);
+   } while (last === randomIndex)
+  
+   last = randomIndex 
+  const randomCatGif = catGifFiles[randomIndex];
+  
+  catGif.src = "assets/" + randomCatGif;
+  catGifContainer.style.display = "block";
+});
